cd8c2a8308458bc78a0e2b16849238183a71dcba
Mattheus Stearns <62345655+Matt-1549@users.noreply.github.com>
2022-03-08 15:41:32 +0000

    finished it, has some errors

---
+++ script.js
@@ -0,0 +1,12 @@
+function myFunction() {
+  document.getElementById("opinion").innerHTML = "PINAPPLE IS BAD BECUASE   IT TASTES really BAD";
+  document.getElementById("opinionimg").src ='https://i.ibb.co/r6Nt27z/Untitled-design-3.png'
+}
+
+function deleteText() {
+  document.getElementById("body").style.color ="gray"
+}
+
+function reinstateText() {
+  document.getElementById("body").style.color ="black"
+}
